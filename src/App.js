08dcea1fe49415adb826694a338fd540274ec148
Amelia Dutta <amelia280111dutta@gmail.com>
2024-08-21 01:08:32 +0530

    completed hero section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Hero from "./components/Hero"
+import divider from "./img/divider.png"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <div className="divider"></div>
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import brandImg from "../img/brand-ice-cream.png"
+export default function Hero(){
+    return(
+        <section className="hero-section">
+            <section className="about-brand">
+                <section className="brand">
+                    <h1 className="brand-name">Luvelicious</h1>
+                    <p className="brand-tag">Dive into a world of creamy perfection, where every bite tells a story of quality and delight.</p>
+                </section>
+                <section className="brand-img">
+                    <img src={brandImg} alt="raspberry icecream"/>
+                </section>
+            </section>
+            <button className="jump-menu">See Menu</button>
+        </section>
+    )
+}
