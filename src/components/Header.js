95a5ea36f3dc9c78e78e8c2b42f41bb159aa6dc9
Amelia Dutta <amelia280111dutta@gmail.com>
2024-08-31 20:12:32 +0530

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { CiMenuKebab } from "react-icons/ci";
+
 export default function Header(){
     return(
         <header>
+            <CiMenuKebab className="hamburger"/>
             <nav className="navbar">
                 <ul className="nav-list">
                     <li className="nav-item"><a href="#">Home</a></li>
